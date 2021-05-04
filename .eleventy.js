e663a630106bb739fb5a70cbf16247cfb9166551
Dave Hearne <dhearne@tssg.org>
2021-05-04 12:07:29 +0100

    add sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const pluginSass = require("eleventy-plugin-sass");
+const eleventyGoogleFonts = require("eleventy-google-fonts");
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(pluginSass);
+    eleventyConfig.addPlugin(eleventyGoogleFonts);
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("admin");
   };
